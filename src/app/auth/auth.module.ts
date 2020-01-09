e0d523fd0d7516e1ee2bdb1dff4c87254f985ae1
cojack <xcojack@gmail.com>
2020-01-09 19:58:26 +0100

    delete(auth): remove unused local passport strategy

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,12 @@ import { AuthService } from './auth.service';
 import { PasswordPipe } from './pipe/password.pipe';
 import { JwtService } from './jwt.service';
 import { CookieStrategy, JwtStrategy } from './strategy';
-import { LocalStrategy } from './strategy/local.strategy';
 
 @Global()
 @Module({
 	imports: [CqrsModule],
 	controllers: [AuthController],
-	providers: [AuthService, JwtService, JwtStrategy, LocalStrategy, CookieStrategy, PasswordPipe],
+	providers: [AuthService, JwtService, JwtStrategy, CookieStrategy, PasswordPipe],
 	exports: [AuthService]
 })
 export class AuthModule {
